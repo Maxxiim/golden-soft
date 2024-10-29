e7549f096137536489042cc8aa513861483b2f13
Maxim <Safronovmaxiim@mail.ru>
2024-10-29 22:34:19 +0300

    The logic for the price slider is complete

--- main.js
+++ main.js
@@ -128,23 +128,50 @@
 //     btnCatalogUnfold.classList.remove('header-nav__list-item-unfold__active');
 // });
 
+
+
+
 // sidebar slider price
 
-const fieldInput = document.querySelectorAll(".sidebar__filters-price-slider-field input");
-const minInput = document.getElementById("minInput");
-const maxInput = document.getElementById("maxInput");
-let priceGap = 1000;
+const rangeInput = document.querySelectorAll('.sidebar__filters-price-slider-range input');
+const progress = document.querySelector('.sidebar__filters-price-slider-controller-progress');
+const priceInput = document.querySelectorAll('.sidebar__filters-price-slider-field input');
 
-fieldInput.forEach((input) => {
+let priceGap = 15000;
 
+priceInput.forEach((input) => {
     input.addEventListener('input', (e) => {
-        let minPrice = fieldInput[0].value;
-        let maxPrice = fieldInput[1].value;
-    });
+        let minRange = parseInt(priceInput[0].value);
+        let maxRange = parseInt(priceInput[1].value);
 
-
+        if ((maxRange - minRange >= priceGap) && maxRange <= 100000) {
+            if (e.target.className === 'sidebar__filters-price-slider-field-min') {
+                rangeInput[0].value = minRange;
+                progress.style.left = (minRange / rangeInput[0].max) * 100 + "%";
+            } else {
+                rangeInput[1].value = maxRange;
+                progress.style.right = 100 - (maxRange / rangeInput[1].max) * 100 + "%";
+            }
+        }
+    })
 })
 
-minInput.addEventListener('input', (e) => {
+rangeInput.forEach((input) => {
+    input.addEventListener('input', (e) => {
+        let minRange = parseInt(rangeInput[0].value);
+        let maxRange = parseInt(rangeInput[1].value);
 
+        if (maxRange - minRange < priceGap) {
+            if (e.target.id === 'minInput') {
+                rangeInput[0].value = maxRange - priceGap;
+            } else {
+                rangeInput[1].value = minRange + priceGap;
+            }
+        } else {
+            priceInput[0].value = minRange;
+            priceInput[1].value = maxRange;
+            progress.style.left = (minRange / rangeInput[0].max) * 100 + "%";
+            progress.style.right = 100 - (maxRange / rangeInput[1].max) * 100 + "%";
+        }
+    })
 })
