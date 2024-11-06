5d22038238731476e787012dd9931739b0b56a04
Maxim <Safronovmaxiim@mail.ru>
2024-11-06 22:53:50 +0300

    Draw dropdown( dropdown-modal added)

--- js/main.js
+++ js/main.js
@@ -2,216 +2,221 @@ import products from "./products";
 
 import paginate from "./modules/pagination";
 
-window.addEventListener('DOMContentLoaded', ()=> {
+window.addEventListener('DOMContentLoaded', () => {
     const productList = document.querySelector('.catalog-product__list');
 
     paginate(products)
 })
 
 
-// const cards = document.querySelectorAll('.product-list__item');
-// const btnLeft = document.querySelector('.controls__arrow-left');
-// const btnRight = document.querySelector('.controls__arrow-right');
-// const dots = document.querySelectorAll(".controls__dots-item");
-// let currentIndex = 1;
 
-// function showCurrentProduct() {
-//     dots.forEach((el) => el.classList.remove('controls__dots-item-active'));
-//     cards.forEach((el) => { el.classList.remove("product-list__item-active") });
-//     cards[currentIndex].classList.add("product-list__item-active");
-//     dots[currentIndex].classList.add('controls__dots-item-active')
-// }
+// header-modal index.html
 
-// function showNextProduct() {
-//     currentIndex++;
-//     if (currentIndex >= cards.length) {
-//         currentIndex = 0;
-//     };
-//     showCurrentProduct();
-// };
+const headerModal = document.querySelector('.header-modal');
+const btnCatalog = document.querySelector('.header-nav__list-item-catalog');
+const btnCatalogUnfold = document.querySelector(".header-nav__list-item-unfold")
+const listItems = document.querySelectorAll('.header-modal__list-item');
 
-// function showPrevProduct() {
-//     currentIndex--;
-//     if (currentIndex < 0) {
-//         currentIndex = cards.length - 1;
-//     };
-//     showCurrentProduct();
-// };
+btnCatalog.addEventListener('mouseenter', () => {
+    headerModal.style.display = 'block';
+    btnCatalogUnfold.classList.add('header-nav__list-item-unfold__active');
+});
 
-// btnLeft.addEventListener('click', showPrevProduct);
-// btnRight.addEventListener('click', showNextProduct);
-// showCurrentProduct();
+btnCatalog.addEventListener('mouseenter', () => {
+    listItems.forEach(item => {
+        item.addEventListener('mouseenter', () => {
+            listItems.forEach(innerItem => {
+                innerItem.querySelector('p').classList.remove('header-modal__list-item-name__active');
+                innerItem.querySelector('img').style.display = 'none'; // Скрываем изображение
+            });
 
-// // product slider
+            const nameElement = item.querySelector('p');
+            nameElement.classList.add('header-modal__list-item-name__active');
 
-// let position = 0;
-// let slidesToShow = 4;
-// let slidesToScroll = 4;
+            const imageElement = item.querySelector('img');
+            imageElement.style.display = 'block';
+        });
 
-// const productContainer = document.querySelector('.product-carousel');
-// const productList = document.querySelector('.product-carousel__list');
-// const productItem = document.querySelectorAll('.product-carousel__list-item');
-// const itemCount = productItem.length;
-// const itemWidth = productContainer.clientWidth / slidesToShow;
-// let movePosition = slidesToScroll * itemWidth;
+    });
+});
 
-// const prevBtn = document.querySelector(".popular__arrow-left");
-// const nextBtn = document.querySelector(".popular__arrow-right");
+headerModal.addEventListener('mouseleave', () => {
+    headerModal.style.display = 'none';
+    btnCatalogUnfold.classList.remove('header-nav__list-item-unfold__active');
+});
 
-// productItem.forEach((el) => {
-//     el.style.minWindth = `${itemWidth}px`;
-// });
+// ====================================================================================
+// slider product-list index.html
+const cards = document.querySelectorAll('.product-list__item');
+const btnLeft = document.querySelector('.controls__arrow-left');
+const btnRight = document.querySelector('.controls__arrow-right');
+const dots = document.querySelectorAll(".controls__dots-item");
+let currentIndex = 1;
 
-// nextBtn.addEventListener('click', () => {
-//     const itemsLeft = itemCount - (Math.abs(position) + slidesToShow + itemWidth) / itemWidth;
+function showCurrentProduct() {
+    dots.forEach((el) => el.classList.remove('controls__dots-item-active'));
+    cards.forEach((el) => { el.classList.remove("product-list__item-active") });
+    cards[currentIndex].classList.add("product-list__item-active");
+    dots[currentIndex].classList.add('controls__dots-item-active')
+}
 
-//     position -= itemsLeft >= slidesToScroll ? movePosition : itemsLeft = itemWidth;
-//     setPosition();
-//     checkBtns();
-// });
+function showNextProduct() {
+    currentIndex++;
+    if (currentIndex >= cards.length) {
+        currentIndex = 0;
+    };
+    showCurrentProduct();
+};
 
-// prevBtn.addEventListener('click', () => {
-//     const itemsLeft = Math.abs(position) / itemWidth;
+function showPrevProduct() {
+    currentIndex--;
+    if (currentIndex < 0) {
+        currentIndex = cards.length - 1;
+    };
+    showCurrentProduct();
+};
 
-//     position += itemsLeft >= slidesToScroll ? movePosition : itemsLeft = itemWidth;
-//     setPosition();
-//     checkBtns();
-// });
+btnLeft.addEventListener('click', showPrevProduct);
+btnRight.addEventListener('click', showNextProduct);
+showCurrentProduct();
 
+// ====================================================================================
 
-// const setPosition = () => {
-//     productList.style.transform = `translateX(${position}px)`;
-// };
+// product slider index.html
 
-// const checkBtns = () => {
-//     prevBtn.disabled = position === 0;
-//     nextBtn.disabled = position <= (itemCount - slidesToShow) * itemWidth;
+let position = 0;
+let slidesToShow = 4;
+let slidesToScroll = 4;
 
-//     if (position === 0) {
-//         prevBtn.classList.add("popular__arrow__grey");
-//         prevBtn.classList.remove("popular__arrow-left");
-//     } else {
-//         prevBtn.classList.remove("popular__arrow__grey");
-//         prevBtn.classList.add("popular__arrow-left");
-//     }
+const productContainer = document.querySelector('.product-carousel');
+const productList = document.querySelector('.product-carousel__list');
+const productItem = document.querySelectorAll('.product-carousel__list-item');
+const itemCount = productItem.length;
+const itemWidth = productContainer.clientWidth / slidesToShow;
+let movePosition = slidesToScroll * itemWidth;
 
-//     if (position <= -((itemCount - slidesToShow) * itemWidth)) {
-//         nextBtn.classList.remove("popular__arrow-right");
-//         nextBtn.classList.add("popular__arrow__grey");
-//     } else {
-//         nextBtn.classList.remove("popular__arrow__grey");
-//         nextBtn.classList.add("popular__arrow-right");
-//     }
-// };
+const prevBtn = document.querySelector(".popular__arrow-left");
+const nextBtn = document.querySelector(".popular__arrow-right");
 
-// // header-modal 
+productItem.forEach((el) => {
+    el.style.minWindth = `${itemWidth}px`;
+});
 
-// const headerModal = document.querySelector('.header-modal');
-// const btnCatalog = document.querySelector('.header-nav__list-item-catalog');
-// const btnCatalogUnfold = document.querySelector(".header-nav__list-item-unfold")
-// const listItems = document.querySelectorAll('.header-modal__list-item');
+nextBtn.addEventListener('click', () => {
+    const itemsLeft = itemCount - (Math.abs(position) + slidesToShow + itemWidth) / itemWidth;
 
-// btnCatalog.addEventListener('mouseenter', () => {
-//     headerModal.style.display = 'block';
-//     btnCatalogUnfold.classList.add('header-nav__list-item-unfold__active');
-// });
+    position -= itemsLeft >= slidesToScroll ? movePosition : itemsLeft = itemWidth;
+    setPosition();
+    checkBtns();
+});
 
-// btnCatalog.addEventListener('mouseenter', () => {
-//     listItems.forEach(item => {
-//         item.addEventListener('mouseenter', () => {
-//             listItems.forEach(innerItem => {
-//                 innerItem.querySelector('p').classList.remove('header-modal__list-item-name__active');
-//                 innerItem.querySelector('img').style.display = 'none'; // Скрываем изображение
-//             });
+prevBtn.addEventListener('click', () => {
+    const itemsLeft = Math.abs(position) / itemWidth;
 
-//             const nameElement = item.querySelector('p');
-//             nameElement.classList.add('header-modal__list-item-name__active');
-
-//             const imageElement = item.querySelector('img');
-//             imageElement.style.display = 'block';
-//         });
-
-//     });
-// });
-
-// headerModal.addEventListener('mouseleave', () => {
-//     headerModal.style.display = 'none';
-//     btnCatalogUnfold.classList.remove('header-nav__list-item-unfold__active');
-// });
+    position += itemsLeft >= slidesToScroll ? movePosition : itemsLeft = itemWidth;
+    setPosition();
+    checkBtns();
+});
 
 
-// const showBlocks = document.querySelectorAll('.catalog-sidebar__filters-item-block');
+const setPosition = () => {
+    productList.style.transform = `translateX(${position}px)`;
+};
 
-// showBlocks.forEach(block => {
+const checkBtns = () => {
+    prevBtn.disabled = position === 0;
+    nextBtn.disabled = position <= (itemCount - slidesToShow) * itemWidth;
 
-//     block.addEventListener('click', () => {
+    if (position === 0) {
+        prevBtn.classList.add("popular__arrow__grey");
+        prevBtn.classList.remove("popular__arrow-left");
+    } else {
+        prevBtn.classList.remove("popular__arrow__grey");
+        prevBtn.classList.add("popular__arrow-left");
+    }
 
-//         const blockSliderPrice = block.closest('.catalog-sidebar__filters-item').querySelector('.sidebar__filters-price-slider');
-//         const checkboxList = block.closest('.catalog-sidebar__filters-item').querySelector('.sidebar__checkbox-list');
-//         const unfoldSpan = block.querySelector('.catalog-sidebar__filters-item-unfold');
+    if (position <= -((itemCount - slidesToShow) * itemWidth)) {
+        nextBtn.classList.remove("popular__arrow-right");
+        nextBtn.classList.add("popular__arrow__grey");
+    } else {
+        nextBtn.classList.remove("popular__arrow__grey");
+        nextBtn.classList.add("popular__arrow-right");
+    }
+};
 
-//         if (blockSliderPrice) {
-//             if (blockSliderPrice.classList.contains('sidebar__filters-price-slider__active')) {
-//                 blockSliderPrice.classList.remove('sidebar__filters-price-slider__active');
-//                 unfoldSpan.classList.remove('catalog-sidebar__filters-item-unfold__active');
-//             } else {
-//                 blockSliderPrice.classList.add('sidebar__filters-price-slider__active');
-//                 unfoldSpan.classList.add('catalog-sidebar__filters-item-unfold__active');
-//             };
-//         };
-//         if (checkboxList) {
-//             if (checkboxList.classList.contains('sidebar__active')) {
-//                 checkboxList.classList.remove('sidebar__active');
-//                 unfoldSpan.classList.remove('catalog-sidebar__filters-item-unfold__active');
-//             } else {
-//                 checkboxList.classList.add('sidebar__active');
-//                 unfoldSpan.classList.add('catalog-sidebar__filters-item-unfold__active');
-//             };
-//         };
-//     });
-// });
+// sidebar filters category.html
+const showBlocksSidebar = document.querySelectorAll('.catalog-sidebar__filters-item-block');
 
-// // sidebar slider price
+showBlocksSidebar.forEach(block => {
 
-// const rangeInput = document.querySelectorAll('.sidebar__filters-price-slider-range input');
-// const progress = document.querySelector('.sidebar__filters-price-slider-controller-progress');
-// const priceInput = document.querySelectorAll('.sidebar__filters-price-slider-field input');
+    block.addEventListener('click', () => {
 
-// let priceGap = 15000;
+        const blockSliderPrice = block.closest('.catalog-sidebar__filters-item').querySelector('.sidebar__filters-price-slider');
+        const checkboxList = block.closest('.catalog-sidebar__filters-item').querySelector('.sidebar__checkbox-list');
+        const unfoldSpan = block.querySelector('.catalog-sidebar__filters-item-unfold');
 
-// priceInput.forEach((input) => {
-//     input.addEventListener('input', (e) => {
-//         let minRange = parseInt(priceInput[0].value);
-//         let maxRange = parseInt(priceInput[1].value);
+        if (blockSliderPrice) {
+            if (blockSliderPrice.classList.contains('sidebar__filters-price-slider__active')) {
+                blockSliderPrice.classList.remove('sidebar__filters-price-slider__active');
+                unfoldSpan.classList.remove('catalog-sidebar__filters-item-unfold__active');
+            } else {
+                blockSliderPrice.classList.add('sidebar__filters-price-slider__active');
+                unfoldSpan.classList.add('catalog-sidebar__filters-item-unfold__active');
+            };
+        };
+        if (checkboxList) {
+            if (checkboxList.classList.contains('sidebar__active')) {
+                checkboxList.classList.remove('sidebar__active');
+                unfoldSpan.classList.remove('catalog-sidebar__filters-item-unfold__active');
+            } else {
+                checkboxList.classList.add('sidebar__active');
+                unfoldSpan.classList.add('catalog-sidebar__filters-item-unfold__active');
+            };
+        };
+    });
+});
 
-//         if ((maxRange - minRange >= priceGap) && maxRange <= 100000) {
-//             if (e.target.className === 'sidebar__filters-price-slider-field-min') {
-//                 rangeInput[0].value = minRange;
-//                 progress.style.left = (minRange / rangeInput[0].max) * 100 + "%";
-//             } else {
-//                 rangeInput[1].value = maxRange;
-//                 progress.style.right = 100 - (maxRange / rangeInput[1].max) * 100 + "%";
-//             }
-//         }
-//     })
-// });
+// sidebar slider price
 
-// rangeInput.forEach((input) => {
-//     input.addEventListener('input', (e) => {
-//         let minRange = parseInt(rangeInput[0].value);
-//         let maxRange = parseInt(rangeInput[1].value);
+const rangeInput = document.querySelectorAll('.sidebar__filters-price-slider-range input');
+const progress = document.querySelector('.sidebar__filters-price-slider-controller-progress');
+const priceInput = document.querySelectorAll('.sidebar__filters-price-slider-field input');
 
-//         if (maxRange - minRange < priceGap) {
-//             if (e.target.id === 'minInput') {
-//                 rangeInput[0].value = maxRange - priceGap;
-//             } else {
-//                 rangeInput[1].value = minRange + priceGap;
-//             }
-//         } else {
-//             priceInput[0].value = minRange;
-//             priceInput[1].value = maxRange;
-//             progress.style.left = (minRange / rangeInput[0].max) * 100 + "%";
-//             progress.style.right = 100 - (maxRange / rangeInput[1].max) * 100 + "%";
-//         }
-//     })
-// });
+let priceGap = 15000;
+
+priceInput.forEach((input) => {
+    input.addEventListener('input', (e) => {
+        let minRange = parseInt(priceInput[0].value);
+        let maxRange = parseInt(priceInput[1].value);
+
+        if ((maxRange - minRange >= priceGap) && maxRange <= 100000) {
+            if (e.target.className === 'sidebar__filters-price-slider-field-min') {
+                rangeInput[0].value = minRange;
+                progress.style.left = (minRange / rangeInput[0].max) * 100 + "%";
+            } else {
+                rangeInput[1].value = maxRange;
+                progress.style.right = 100 - (maxRange / rangeInput[1].max) * 100 + "%";
+            }
+        }
+    })
+});
+
+rangeInput.forEach((input) => {
+    input.addEventListener('input', (e) => {
+        let minRange = parseInt(rangeInput[0].value);
+        let maxRange = parseInt(rangeInput[1].value);
+
+        if (maxRange - minRange < priceGap) {
+            if (e.target.id === 'minInput') {
+                rangeInput[0].value = maxRange - priceGap;
+            } else {
+                rangeInput[1].value = minRange + priceGap;
+            }
+        } else {
+            priceInput[0].value = minRange;
+            priceInput[1].value = maxRange;
+            progress.style.left = (minRange / rangeInput[0].max) * 100 + "%";
+            progress.style.right = 100 - (maxRange / rangeInput[1].max) * 100 + "%";
+        }
+    })
+});
