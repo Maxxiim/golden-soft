c5368a65340c4a49ade0aeba7218eb949ef0c26a
Maxim <Safronovmaxiim@mail.ru>
2024-11-18 22:53:37 +0300

    Added functions for showing blocks (specifications, description, reviews), function for product-slider (selection of active picture)

--- js/product.js
+++ js/product.js
@@ -1,3 +1,19 @@
+const productMainImage = document.querySelector('.product-slider__main');
+const productThumbnailsImages = document.querySelectorAll('.product-slider__thumbnails-list-item img');
+productThumbnailsImages.forEach((thumbnail) => {
+    thumbnail.addEventListener('click', () => {
+        const newSrc = thumbnail.src.replace('thumbnails', 'main');
+        console.log(newSrc)
+
+        productMainImage.src = newSrc;
+
+        productThumbnailsImages.forEach((item) => item.parent.classList.remove('product-slider__main-img__active'));
+
+        thumbnail.parentNode.classList.add('product-slider__main-img__active')
+    });
+});
+// ================================================================
+
 const fieldPackageModal = document.querySelector('.product-info__options-package-content');
 const packageModalUnfoldIcon = document.querySelector('.product-info__options-package-content-unfold');
 const packageModal = document.querySelector('.package-modal');
@@ -67,11 +83,48 @@ colorSelection.forEach((color) => {
 // Services sections
 const serviceItems = document.querySelectorAll('.product-info__services-list-item');
 serviceItems.forEach((item) => {
-  const title = item.querySelector('.product-info__services-list-item-title');
-  const description = item.querySelector('.product-info__services-list-item-descr');
-  const iconUnfold = item.querySelector('.product-info__services-list-item-unfold');
-  title.addEventListener('click', () => {
-    description.classList.toggle('product-info__services-list-item-descr__active');
-    iconUnfold.classList.toggle('product-info__services-list-item-unfold__active');
-  });
+    const title = item.querySelector('.product-info__services-list-item-title');
+    const description = item.querySelector('.product-info__services-list-item-descr');
+    const iconUnfold = item.querySelector('.product-info__services-list-item-unfold');
+    title.addEventListener('click', () => {
+        description.classList.toggle('product-info__services-list-item-descr__active');
+        iconUnfold.classList.toggle('product-info__services-list-item-unfold__active');
+    });
 });
+
+// ================================================================
+// details section
+
+const detailsItem = document.querySelectorAll('.product-details__list-item');
+const blockCharacteristic = document.querySelector('.product-details__content-characteristics');
+const blockDescr = document.querySelector('.product-details__content-descr')
+const blockReviews = document.querySelector('.product-details__content-reviews');
+
+detailsItem.forEach((item) => {
+    item.addEventListener('click', (e) => {
+
+        detailsItem.forEach((el) => el.classList.remove('product-details__list-item__active'));
+
+        item.classList.add('product-details__list-item__active');
+
+
+        if (e.target.innerText === 'Характеристики') {
+            blockCharacteristic.classList.add('product-details__content-characteristics__active');
+            blockDescr.classList.remove('product-details__content-descr__active');
+            blockReviews.classList.remove('product-details__content-reviews__active');
+        }
+
+        if (e.target.innerText === 'Описание') {
+            blockDescr.classList.add('product-details__content-descr__active');
+            blockCharacteristic.classList.remove('product-details__content-characteristics__active');
+            blockReviews.classList.remove('product-details__content-reviews__active');
+        }
+
+        if (e.target.innerText === 'Отзывы') {
+            blockReviews.classList.add('product-details__content-reviews__active');
+            blockDescr.classList.remove('product-details__content-descr__active');
+            blockCharacteristic.classList.remove('product-details__content-characteristics__active');
+        }
+
+    })
+})
